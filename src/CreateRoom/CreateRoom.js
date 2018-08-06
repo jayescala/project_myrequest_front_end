e3b4cbb1d7ee7e9b720b4a1371e6ddcef0089de2
Alex Delgado <delgadoalex@icloud.com>
2018-08-06 12:45:57 -0500

    CreateRoom component created

---
+++ src/CreateRoom/CreateRoom.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react'; 
+
+class CreateRoom extends Component {
+  constructor() {
+    super(); 
+    this.state = {
+      code: '', 
+      createdByUsername: '', 
+      createdByUserId: '', 
+      comments: '', 
+      pendingRequest: '', 
+      approvedRequest: '', 
+      roomName: '', 
+      description: '', 
+      image: ''
+    }
+  }
+}
+
+  render() {
+    console.log(this.props, 'this is props'); 
+    return (
+
+      <form onSubmit={this.props.addRoom.bind(this, this.state)}>
+       <input type="submit" />
+      </form>
+    )
+  }
+
+
+  export default CreateRoom;
