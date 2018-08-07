385c62d6bc4d98354e3ba26f079031b6c7a0258f
Alex Delgado <delgadoalex@icloud.com>
2018-08-07 09:57:12 -0500

    Room Container complete

---
+++ src/RoomContainer/Room.js
@@ -0,0 +1,89 @@
+import React, {Component} from 'react'; 
+import CreateRoom from '../CreateRoom'; 
+
+class Room extends Component {
+  constructor() {
+    super(); 
+    this.state = {
+      code: '', 
+      createdByUsername: '', 
+      createdByUserId: '', 
+      comments: '', 
+      pendingRequest: '', 
+      approvedRequest: '', 
+      roomName: '', 
+      description: '',
+      image: ''     
+    }
+  }
+}
+
+componentDidMount() {
+  this.getRoom().then((Room) => {
+    this.setState({
+      Room: Room.data
+    }); 
+  }).catch((err) => {
+    console.log(err, 'this is an error in the componentDidMount');
+  });
+}
+
+getRoom = async () => {
+  const room = await fetch('http://localhost:9000/api/v1/rooms', {
+    credentials: 'include',
+    method: 'GET'
+  });  
+
+  const parsedRoom = Room.json();
+  return parsedRoom
+}
+
+addRoom = async (room, e) => {
+  e.preventDefault();
+  try {
+    const createRoom = await fetch('http://localhost:9000/api/v1/rooms', {
+      method: 'POST',
+      credentials: 'include',
+      body: JSON.stringify(room),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+
+    const parsedResponse = await createRoom.json();
+
+    this.state({room: [...this.state.room, parsedResponse.data]})
+  } catch(err) {
+    console.log(err)
+  }
+}
+
+  deleteRoom = async (id, e) => {
+    e.preventDefault();
+    console.log('deleteRoom function is being called, this is the id:', id);
+    try {
+      const deleteRoom = await fetch('http://localhost:9000/api/v1/room/' + id, {
+        method: 'DELETE'
+      });
+
+      const parsedRoom = await deleteRoom.json();
+      this.setState({room: this.state.room.filter((room, i) => room._id)});
+
+    } catch(err) {
+      console.log(err);
+
+    }
+  
+  }
+
+  render() {
+    return (
+      <div>
+        <Room Room={this.state.Room} deleteRoom={this.deleteRoom} />
+        <CreateRoom addRoom={this.addRoom} />
+      </div>
+    )
+  }
+
+
+  export default Room;
